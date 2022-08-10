376dd8e6746f61a33a767d6bbdaf00c0f4283129
NikitaP <unntiss@bk.ru>
2022-08-10 18:52:37 +0300

    HW_13

---
+++ HW_13/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+
+
+while (num > 999)
+{
+    num /= 10;
+}
+if (num > 100)
+{
+    int result = num % 10;
+    Console.WriteLine(result);
+}
+else Console.WriteLine("There is no 3rd number");
