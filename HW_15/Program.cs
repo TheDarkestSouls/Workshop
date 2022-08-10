591f97ccdf80fe3b630dbbdb1ba2daca5044a656
NikitaP <unntiss@bk.ru>
2022-08-10 19:09:13 +0300

    HW_15

---
+++ HW_15/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+
+if(num == 6 || num == 7)
+{
+    Console.WriteLine(" is weekend!");
+}
+else if (num > 0 && num < 6) 
+Console.WriteLine(" is not weekend.");
+else 
+Console.WriteLine(" is not found in week.");
