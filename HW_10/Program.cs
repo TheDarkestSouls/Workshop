43bd592eea515d492f30a20860faab56e80999dd
NikitaP <unntiss@bk.ru>
2022-08-10 18:12:10 +0300

    HW_10

---
+++ HW_10/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+
+    // int temp = num % 100;
+    // int result = temp / 10;
+
+    // Console.WriteLine(result);
+
+    Console.WriteLine((num % 100) / 10);
+}
+else Console.WriteLine("Something wrong...");
