df887f49e4eded8d443377b7c58aecac65e26c20
NikitaP <unntiss@bk.ru>
2022-08-16 18:18:44 +0300

    HW_23

---
+++ HW_23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Cube numbers from 1...N
+
+Console.Write("Enter the number: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine();
+for(int i = 1; i <= N; i++) // cycle
+{
+    Console.WriteLine(Math.Pow(i, 3)); // cube
+    Console.WriteLine();
+}
