6ac57b8f44a108ac6ccf6db48c0b7976a50a3e7b
NikitaP <unntiss@bk.ru>
2022-08-16 16:37:34 +0300

    HW_19

---
+++ HW_19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Is number palindrome or not
+int n, r, sum = 0, temp; // n = number, r = right symbol of the number, sum = mirror of the number, temp = temporal integer
+Console.Writeline("Enter the Number: ");
+n = Convert.ToInt32(Console.Readline());
+temp = n;
+while (n > 0)
+{
+    r = n % 10; // finding the right symbol
+    sum = (sum * 10) + r; // assembling the mirror
+    n = n / 10; // cutting the right symbol
+}
+if (temp == sum)
+Console.Writeline("Number is Palindrome");
+else 
+Console.Writeline("Number is not Palindrome");
+
