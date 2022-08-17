dfa19dafe85911713280fe5744736350c4140af9
NikitaP <unntiss@bk.ru>
2022-08-17 16:55:07 +0300

    HW_28, HW_30 modified

--- HW_28/Program.cs
+++ HW_28/Program.cs
@@ -1,7 +1,7 @@
 ﻿int N = new Random().Next(2, 11);
-
+int result = 1;
 for (int i = 1; i <= N; i++)
 {
-int result = Convert.ToInt32(Math.Pow(i, 3));
-Console.Write(result + " ");
+result *= i;
 }
+Console.Write(N + " " + result);
