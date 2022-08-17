97430b30a9b1d220f69ce054d658a7962a3ac269
NikitaP <unntiss@bk.ru>
2022-08-17 16:48:16 +0300

    HW_28, HW_30

---
+++ HW_28/Program.cs
@@ -0,0 +1,7 @@
+﻿int N = new Random().Next(2, 11);
+
+for (int i = 1; i <= N; i++)
+{
+int result = Convert.ToInt32(Math.Pow(i, 3));
+Console.Write(result + " ");
+}
---
+++ HW_30/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] arr = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+arr[i] = new Random().Next(0, 2);
+Console.Write(arr[i] + " ");
+}
