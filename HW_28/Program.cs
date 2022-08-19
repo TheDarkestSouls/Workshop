280eef60ab0a56137b9e946cb719a860357d2537
NikitaP <unntiss@bk.ru>
2022-08-20 02:47:39 +0300

    28_30

--- HW_28/Program.cs
+++ HW_28/Program.cs
@@ -1,7 +1,7 @@
-﻿int N = new Random().Next(2, 11);
+﻿int N = new Random().Next(2, 11); //some numbers from 2 to 11
 int result = 1;
 for (int i = 1; i <= N; i++)
 {
 result *= i;
 }
-Console.Write(N + " " + result);
+Console.Write(N + " " + result); // show number and product of all its numbers
--- HW_30/Program.cs
+++ HW_30/Program.cs
@@ -2,6 +2,6 @@
 
 for (int i = 0; i < 8; i++)
 {
-arr[i] = new Random().Next(0, 2);
+arr[i] = new Random().Next(0, 2); // generate array of 0 and 1 randomly ordered
 Console.Write(arr[i] + " ");
 }
