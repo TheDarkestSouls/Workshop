632801cf854cef75627bc1e3dcb4f53667d42988
NikitaP <unntiss@bk.ru>
2022-08-20 03:02:06 +0300

    25

---
+++ HW_25/Program.cs
@@ -0,0 +1,12 @@
+﻿                            // A raise to the power of B
+                            
+int a = Convert.ToInt32(new Random().Next(2, 9)); // random number from 2 to 9
+int b = Convert.ToInt32(new Random().Next(2, 9)); // random number from 2 to 9
+int temp = a;
+Console.Write(a + " raise to the power of " + b + " equal ");
+
+for(int i = 1; i < b; i++)
+{
+    a *= temp;
+}
+Console.Write(a);
