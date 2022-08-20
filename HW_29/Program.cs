48f1cbcececf9e4c477bfa40042b4ed382445d4f
NikitaP <unntiss@bk.ru>
2022-08-20 05:06:19 +0300

    HW_29

---
+++ HW_29/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] arr = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    arr[i] = int.Parse(Console.ReadLine()); // generate array of 0 and 1 randomly ordered
+}
+for(int i = 0; i < 8; i++)
+{
+    Console.Write(arr[i] + " ");
+}
