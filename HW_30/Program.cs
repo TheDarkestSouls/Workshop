3017c7511ae09d98a2f1f629d167ab1b691dee44
NikitaP <unntiss@bk.ru>
2022-08-22 05:09:18 +0300

    add function to HW_30

--- HW_30/Program.cs
+++ HW_30/Program.cs
@@ -5,3 +5,15 @@ for (int i = 0; i < 8; i++)
 arr[i] = new Random().Next(0, 2); // generate array of 0 and 1 randomly ordered
 Console.Write(arr[i] + " ");
 }
+
+// or using the function below
+
+int[] RandomArray(int N, int start, int end)
+{
+    int[] array = new int[N];
+    for(int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
