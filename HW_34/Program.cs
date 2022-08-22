128fb4ad148c7a9e37e55d6cd02724941a86144f
NikitaP <unntiss@bk.ru>
2022-08-23 02:24:21 +0300

    HW_34

---
+++ HW_34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+int[] RandomArray = new int[N];
+for (int i = 0; i < N; i++)
+{
+RandomArray[i] = new Random().Next(start, end + 1);
+}
+return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+int evensQ(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) sum++;
+    }
+    return sum;
+}
+
+int[] array = CreateRandomArray(8, 100, 999);
+ShowArray(array);
+Console.WriteLine(evensQ(array));
