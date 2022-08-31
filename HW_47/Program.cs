e50c7b896b8a35311408188df5f99d6ff4d3ecb7
NikitaP <unntiss@bk.ru>
2022-08-31 14:13:47 +0300

    47

---
+++ HW_47/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 47. Задайте двумерный массив размером m × n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.WriteLine("Enter the amount of rows: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the amount of columns: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = getArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] getArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// void ShowArray(int[] array)
+// {
+// for (int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// }
