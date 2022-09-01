fc5479be8aeb616c85d6a26b68ca60f456678f99
NikitaP <unntiss@bk.ru>
2022-09-01 22:01:55 +0300

    54

--- HW_54/Program.cs
+++ HW_54/Program.cs
@@ -8,3 +8,34 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] array = new int[5, 5];
+
+for (int i = 0; i < array.GetLength(0); i++) // Generate random array
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 9);
+    }
+}
+
+//From max to min variance
+
+void SelectionSort(int[,] array)
+{
+    for (int k = 0; k < array.GetLength(0); k++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            int maxPosition = i;
+
+            for (int j = i + 1; j < array.GetLength(1); j++)
+            {
+                if (array[k, j] > array[k, maxPosition]) maxPosition = j; 
+            }
+
+            int temp = array[k, i];
+            array[k, i] = array[k, maxPosition];
+            array[k, maxPosition] = temp;
+        }
+    }
+}
