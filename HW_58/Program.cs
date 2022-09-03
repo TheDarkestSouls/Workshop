a74f0b0ea5521949c576e2171c8c46a093f4e227
NikitaP <unntiss@bk.ru>
2022-09-03 17:45:13 +0300

    58

--- HW_58/Program.cs
+++ HW_58/Program.cs
@@ -6,3 +6,69 @@
 // 18 20
 // 15 18
 
+Console.Clear();
+
+Console.Write("Enter amount of first array:");
+int rowsA = int.Parse(Console.ReadLine());
+Console.Write("Enter amount of first array:");
+int columnsA = int.Parse(Console.ReadLine());
+Console.Write("Enter amount of second array:");
+int rowsB = int.Parse(Console.ReadLine());
+Console.Write("Enter amount of second array:");
+int columnsB = int.Parse(Console.ReadLine());
+
+if(columnsA != rowsB)
+{
+    Console.WriteLine("Cant multiple such matrix");
+    return;
+}
+
+int[,] A = GetArray(rowsA, columnsA, 0, 10);
+int[,] B = GetArray(rowsB, columnsB, 0, 10);
+PrintArray(A);
+Console.WriteLine();
+PrintArray(B);
+Console.WriteLine();
+PrintArray(GetMultiplicationMatrix(A, B));
+
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for(int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for(int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for(int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
