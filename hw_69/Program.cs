84bf0d4c3791da4f8b9ac6c562cf869dd55edbad
NikitaP <unntiss@bk.ru>
2022-09-06 18:40:23 +0300

    69

---
+++ hw_69/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = int.Parse(Console.ReadLine());
+
+
+
+int pow(int number, int rank);
+{
+    if (rank == 0) return 1;
+    return (number * pow(number, rank - 1));
+}
