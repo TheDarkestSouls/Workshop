d5d1fc8fe544c3dc4d6255027db5d3697a41091a
NikitaP <unntiss@bk.ru>
2022-09-06 18:15:22 +0300

    65

---
+++ hw_65/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Clear();
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+
+PrintNumbers(M, N);
+
+void PrintNumbers(int x, int y)
+{
+Console.Write($"{x} ");
+if (x == y) return;
+PrintNumbers(x + 1, y);
+}
