4238cad828d7a2a4fd487fb1539ab8eae1274c18
NikitaP <unntiss@bk.ru>
2022-09-06 18:31:25 +0300

    67

---
+++ HW_67/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{m}->{SumNumbers(m)}");
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    if (number == 0) return 0;
+    sum = number % 10 + SumNumbers(number / 10);
+    return sum;
+}
